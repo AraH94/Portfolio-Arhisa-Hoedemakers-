dba845fc914e5b364c466c031148a39290be9520
AraH94 <arahoedemakers@hotmail.com>
2023-05-30 15:52:24 +0200

    update portfolio

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -110,7 +110,7 @@ function Contact() {
 					</motion.div>
 				</div>
 			</div>
-			<iframe className='mt-8' width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=3680%20Maaseik+(My%20place)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe>
+			<iframe className='map mt-8' width="100%" height="500" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=3680%20Maaseik+(My%20place)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">measure distance on map</a></iframe>
 		</section>
 
 	)
